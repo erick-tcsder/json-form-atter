79de33bb6a8cf4eb49eb2b76c720ed9026cddbf2
Erick <erickfons00@gmail.com>
2022-09-17 11:49:43 -0400

    feat(components): fixing imports and scripts

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,5 +1,9 @@
 import { Block } from './types/Block';
 import { Fragment } from './types/Fragment';
-import type { BlockValidation } from './types/Validation';
-export type { BlockValidation as Validation };
-export { Block, Fragment };
+import type { BlockValidation, InputValidation, Validation } from './types/Validation';
+import { Input } from './types/Input';
+import type { FromJSONParser } from "./types/FromJSONParser";
+import { defaultFromJSONParser } from "./types/FromJSONParser";
+export type { BlockValidation, InputValidation, Validation, FromJSONParser };
+export { Block as BlockFragment, Fragment, Input as InputFragment };
+export { defaultFromJSONParser };
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,11 @@
 import {Block} from './types/Block'
 import {Fragment} from './types/Fragment'
-import type {BlockValidation} from './types/Validation'
+import type {BlockValidation,InputValidation,Validation} from './types/Validation'
+import { Input } from './types/Input'
+import type { FromJSONParser } from "./types/FromJSONParser"
+import { defaultFromJSONParser } from "./types/FromJSONParser"
 
-export type {BlockValidation as Validation}
-export {Block,Fragment}
+export type {BlockValidation,InputValidation,Validation,FromJSONParser}
+export {Block as BlockFragment,Fragment,Input as InputFragment}
+
+export {defaultFromJSONParser}
