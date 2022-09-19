bb7bf6e19c1f382eeef82ce681066760d5f65c64
Erick <erickfons00@gmail.com>
2022-09-19 09:37:23 -0400

    fix(lib): removing js compiled files

--- lib/types/Fragment.d.ts
+++
@@ -1,9 +0,0 @@
-import { FromJSONParser } from './FromJSONParser';
-export declare class Fragment {
-    name?: string;
-    _id?: string;
-    FRAGMENT_TYPE: string;
-    constructor(name?: string, _id?: string);
-    toObject(): Record<string, any>;
-    static fromJSON(obj: Record<string, any>, fromJSONParser?: FromJSONParser): Fragment;
-}
